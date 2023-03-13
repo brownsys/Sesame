203599811250d41c07e033240e4e5b7f23dea61c
Artem Agvanian <artemagvanian@icloud.com>
2023-03-13 14:00:34 -0400

    feat: add a manager non-admin into the priming dataset

--- data/prime.sql
+++ data/prime.sql
@@ -3,6 +3,7 @@ INSERT INTO websubmit.users (email, apikey, is_admin, is_manager, pseudonym, gen
 INSERT INTO websubmit.users (email, apikey, is_admin, is_manager, pseudonym, gender, age, ethnicity, is_remote, education) VALUES ('ann@brown.edu', '38f1f0106e87a77a2d5ac1a02b21fafc7f0a66dd9266bc68320260751c64f1b4', 0, 0, 'XXXOOqJM32INbxH1', 'female', 20, '...', 0, 'masters');
 INSERT INTO websubmit.users (email, apikey, is_admin, is_manager, pseudonym, gender, age, ethnicity, is_remote, education) VALUES ('artem@brown.edu', '4424109d9f524aa1cc1fd730097f4c865feb2f827bfa8d1390af58da9dc090e3', 1, 1, 'DfsHoGKHMzmek7Hv', 'male', 19, '...', 0, 'high school');
 INSERT INTO websubmit.users (email, apikey, is_admin, is_manager, pseudonym, gender, age, ethnicity, is_remote, education) VALUES ('sunny@brown.edu', 'b69045df9d15113dd330a3564a71dd2f3eab9a41c0a23e19a55b4f4baa628067', 0, 0, 'Rn6uH3QjLRl1JDec', 'female', 21, '...', 0, 'high school');
+INSERT INTO websubmit.users (email, apikey, is_admin, is_manager, pseudonym, gender, age, ethnicity, is_remote, education) VALUES ('sullivan@brown.edu', '027259162a9102ae872e259a2c20d2263e024586fed8ad8ef5dba6abf4d6e6b2', 0, 1, 'dLtgv9m493hRDBID', 'non-binary', 35, '...', 0, 'PhD');
 -- lectures
 INSERT INTO websubmit.lectures (id, label) VALUES (1, '1');
 -- questions
