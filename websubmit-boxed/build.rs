6745cd8cd5e1c2ee960e3a2cc93d4b292397a6a4
corinnt <94402869+corinnt@users.noreply.github.com>
2024-03-15 18:23:54 +0100

    add compiler message colors to build output

--- websubmit-boxed/build.rs
+++ websubmit-boxed/build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 macro_rules! p {
     ($($tokens: tt)*) => {
-        println!("cargo:warning=\r\x1b[32;1m    {}", format!($($tokens)*))
+        println!("cargo:warning=\r\x1b[32;1m{}", format!($($tokens)*))
     }
 }
 
@@ -10,19 +10,19 @@ fn main() {
     let profile = std::env::var("PROFILE").unwrap();
 
     if profile.clone().as_str() == "release" {
-        p!("\x1b[97mRunning Alohomora lints...\x1b[0m");
+        p!("\x1b[97m    Running Alohomora lints...\x1b[0m");
         let lint_res = Command::new("cargo")
             .arg("dylint")
             .arg("--all")
             .status()
             .expect("cargo dylint failed"); 
         
-        if !lint_res.success() { // color codes on either side for red output
-            panic!("\x1b[91mAlohomora lints failed! See above for manual implementations to remove.\x1b[0m"); 
+        if !lint_res.success() {
+            panic!("\x1b[91merror: \x1b[97malohomora lints failed! See above for manual implementations to replace.\x1b[0m"); 
         } else {
-            p!("\x1b[92mAlohomora lints passed!\x1b[0m");
+            p!("\x1b[92m    Alohomora lints passed!\x1b[0m");
         }
-    } else {
-        p!("\x1b[96mIn {} mode without Alohomora lints\x1b[0m", profile);
-    }
+    } /*else {
+        //p!("\x1b[96mnote: \x1b[97min {} mode without Alohomora lints\x1b[0m", profile);
+    } */
 }
