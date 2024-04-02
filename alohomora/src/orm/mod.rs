19993cdd29c6348b6e18626a8246f47d79aea6d3
Kinan Dak Albab <kinan.bab.14@gmail.com>
2024-04-01 21:20:36 -0400

    Begin work on supporting ORM

---
+++ alohomora/src/orm/mod.rs
@@ -0,0 +1,7 @@
+mod rocket;
+
+pub use rocket::*;
+
+// TODO(babman): we need to override sea_orm::Database,DatabaseConnection
+// TODO(babman): we need to override entites / schema so that it takes BBoxes
+// TODO(babman): we need to override querys and insert API.
