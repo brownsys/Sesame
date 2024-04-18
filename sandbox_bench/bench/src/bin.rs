1b8e9d024a6628a49582a6e1d8c103b3f10cab02
Allen Aby <allen.aby@gmail.com>
2024-04-17 20:19:56 -0400

    timing for hash, write to file

--- sandbox_bench/bench/src/bin.rs
+++ sandbox_bench/bench/src/bin.rs
@@ -1,62 +1,86 @@
 extern crate alohomora;
 extern crate bench_lib;
 
-use std::collections::HashSet;
-
+use std::fs;
 use std::vec;
 
 use alohomora::bbox::BBox;
 use alohomora::policy::NoPolicy;
-use alohomora::pure::PrivacyPureRegion;
 use alohomora::sandbox::execute_sandbox;
 
-use bench_lib::{add_numbers, hash, mult_numbers, train, Numbers};
+use bench_lib::{hash, train};
 
 use chrono::naive::NaiveDateTime;
-use linfa_linear::FittedLinearRegression;
+use chrono::Utc;
+// use linfa_linear::FittedLinearRegression;
 
-fn main() {
-    let bbox = BBox::new(Numbers { a: 4, b: 15 }, NoPolicy {});
-    // START TIMER
-    let bbox = execute_sandbox::<mult_numbers, _, _>(bbox);
-    // END TIMER
-    let bbox = bbox.specialize_policy::<NoPolicy>().unwrap();
-    println!("{}", bbox.discard_box());
-
-    // // PPR.
-    // let set = HashSet::from([10u32, 7u32]);
-    // let bbox = BBox::new(10u32, NoPolicy {});
-    // let bbox = bbox.into_ppr(PrivacyPureRegion::new(|val| set.contains(&val)));
-    // println!("{}", bbox.discard_box());
-
-    // let bbox = BBox::new(5u32, NoPolicy {});
-    // let bbox = bbox.into_ppr(PrivacyPureRegion::new(|val| {
-    //   println!("Buggy leak {}", val);
-    //   set.contains(&val)
-    // }));
-    // println!("{}", bbox.discard_box());
-
+fn hash_bench(iters: u64) -> Vec<(i64, i64)> {
+  (1..iters).map(|_i| {
     let email = BBox::new("allen_aby@brown.edu".to_string(), NoPolicy {});
     let secret = BBox::new("SECRET".to_string(), NoPolicy {});
-    // START TIMER (end inside hash)
-    let hash = execute_sandbox::<hash, _, _>((email, secret));
-    // END TIMER (start inside hash)
-    let hash = hash.specialize_policy::<NoPolicy>().unwrap();
-    println!("{}", hash.discard_box());
 
-    type BBoxTime = BBox<NaiveDateTime, NoPolicy>;
-    type BBoxGrade = BBox<u64, NoPolicy>;
+    // START TIMER (end inside hash)
+    let now = Utc::now();
+    let now = now.timestamp_nanos_opt().unwrap();
+
+    let output: BBox<(i64, String, i64), alohomora::policy::AnyPolicy> = execute_sandbox::<hash, _, _>((email, secret, BBox::new(now, NoPolicy {})));
+    
+    // END TIMER (start inside hash)
+    let now = Utc::now().timestamp_nanos_opt().unwrap();
+
+    let output = output.specialize_policy::<NoPolicy>().unwrap();
+    let output: (i64, String, i64) = output.discard_box();
+    let teardown = now - output.2;
+    (output.0, teardown)
+  }).collect()
+}
+
+fn train_bench(iters: u64) -> Vec<(i64, i64)> {
+  type BBoxTime = BBox<NaiveDateTime, NoPolicy>;
+  type BBoxGrade = BBox<u64, NoPolicy>;
+
+  (1..iters).map(|i| {
+    println!("{}", i);
     let grades: Vec<(BBoxTime, BBoxGrade)> = vec![
       (BBox::new(NaiveDateTime::parse_from_str("2023-03-13 13:40:26", "%Y-%m-%d %H:%M:%S").unwrap(), NoPolicy {}), BBox::new(90 as u64, NoPolicy {})),
       (BBox::new(NaiveDateTime::parse_from_str("2023-03-09 13:54:05", "%Y-%m-%d %H:%M:%S").unwrap(), NoPolicy {}), BBox::new(95 as u64, NoPolicy {})),
     ];
+
     // START TIMER (end inside train)
-    let model = execute_sandbox::<train, _, _>(grades);
-    let time = NaiveDateTime::parse_from_str("2023-03-13 13:40:50", "%Y-%m-%d %H:%M:%S");
-    // END TIMER (start inside train)
-    let grade = model.into_ppr(PrivacyPureRegion::new(|model: FittedLinearRegression<f64>|
-      model.params()[0] * (time.unwrap().and_utc().timestamp() as f64) + model.intercept()
-    ));
-    let grade = grade.specialize_policy::<NoPolicy>().unwrap();
-    println!("{}", grade.discard_box());
+    let now = Utc::now();
+    let now = now.timestamp_nanos_opt().unwrap();
+    println!("reached");
+
+    let output = execute_sandbox::<train, _, _>((grades, now));
+
+    // END TIMER (start inside hash)
+    let now = Utc::now().timestamp_nanos_opt().unwrap();
+
+    let output = output.specialize_policy::<NoPolicy>().unwrap();
+    let output = output.discard_box();
+    let teardown = now - output.2;
+    (output.0, teardown)
+    // let time = NaiveDateTime::parse_from_str("2023-03-13 13:40:50", "%Y-%m-%d %H:%M:%S");
+    // let grade = model.into_ppr(PrivacyPureRegion::new(|model: FittedLinearRegression<f64>|
+    //   model.params()[0] * (time.unwrap().and_utc().timestamp() as f64) + model.intercept()
+    // ));
+    // let grade = grade.specialize_policy::<NoPolicy>().unwrap();
+  }).collect()
+}
+
+fn write_stats(name: String, data: Vec<(i64, i64)>) {
+  fs::create_dir_all("benches/").unwrap();
+  fs::write(
+      format!("benches/{}.json", name),
+      serde_json::to_string_pretty(&data).unwrap(),
+  )
+  .unwrap();
+}
+
+fn main() {
+  let hash_res = hash_bench(100);
+  write_stats("hash".to_string(), hash_res);
+
+  // let train_res = train_bench(100);
+  // write_stats("train".to_string(), train_res);
 }
--- sandbox_bench/bench_lib/src/lib.rs
+++ sandbox_bench/bench_lib/src/lib.rs
@@ -1,64 +1,57 @@
 use alohomora_derive::AlohomoraSandbox;
 
 use chrono::naive::NaiveDateTime;
+use chrono::Utc;
 use linfa::dataset::Dataset;
 use linfa::prelude::*;
 use linfa_linear::{FittedLinearRegression, LinearRegression};
 use ndarray::prelude::*;
-use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
 
-// Argument type for sandboxes.
-#[derive(Serialize, Deserialize)]
-pub struct Numbers {
-    pub a: u32,
-    pub b: u32,
-}
 
 // Sandbox functions.
 #[AlohomoraSandbox()]
-pub fn add_numbers(n: Numbers) -> u32 {
-    // END TIMER
-    n.a + n.b
-    // START TIMER
+pub fn hash(inputs: (String, String, i64)) -> (i64, String, i64) {
+  // END TIMER (start in bin)
+  let now = Utc::now().timestamp_nanos_opt().unwrap();
+  let setup = now - inputs.2;
+
+  let mut hasher = Sha256::new();
+  hasher.update(&inputs.0);
+  hasher.update(&inputs.1);
+  let key = format!("{:x}", hasher.finalize());
+
+  // START TIMER (end in bin)
+  let now = Utc::now().timestamp_nanos_opt().unwrap();
+  (setup, key, now)
 }
 
 #[AlohomoraSandbox()]
-pub fn mult_numbers(n: Numbers) -> u32 {
-    n.a * n.b
-}
+pub fn train(inputs: (Vec<(NaiveDateTime, u64)>, i64)) -> (i64, FittedLinearRegression<f64>, i64) {
+  println!("reached");
+  // END TIMER (start in bin)
+  let now = Utc::now().timestamp_nanos_opt().unwrap();
+  let setup = now - inputs.1;
 
-#[AlohomoraSandbox()]
-pub fn hash(inputs: (String, String)) -> String {
-    // END TIMER (start in bin)
-    let mut hasher = Sha256::new();
-    hasher.update(&inputs.0);
-    hasher.update(&inputs.1);
-    format!("{:x}", hasher.finalize())
-    // START TIMER (end in bin)
-}
+  let grades = inputs.0;
+  let grades: Vec<[f64; 2]> = grades
+      .into_iter()
+      .map(|g| [g.0.clone().and_utc().timestamp() as f64, g.1 as f64])
+      .collect();
+  let array: Array2<f64> = Array2::from(grades);
+  let (x, y) = (
+      array.slice(s![.., 0..1]).to_owned(),
+      array.column(1).to_owned(),
+  );
 
-#[AlohomoraSandbox()]
-pub fn train(grades: Vec<(NaiveDateTime, u64)>) -> FittedLinearRegression<f64> {
-    // END TIMER (start in bin)
-    let grades: Vec<[f64; 2]> = grades
-        .into_iter()
-        .map(|g| [g.0.clone().and_utc().timestamp() as f64, g.1 as f64])
-        .collect();
-    let array: Array2<f64> = Array2::from(grades);
-    let (x, y) = (
-        array.slice(s![.., 0..1]).to_owned(),
-        array.column(1).to_owned(),
-    );
+  let dataset: Dataset<f64, f64, Dim<[usize; 1]>> =
+      Dataset::new(x, y).with_feature_names(vec!["x", "y"]);
 
-    let dataset: Dataset<f64, f64, Dim<[usize; 1]>> =
-        Dataset::new(x, y).with_feature_names(vec!["x", "y"]);
+  // Train the model.
+  let lin_reg = LinearRegression::new();
+  let model = lin_reg.fit(&dataset).unwrap();
 
-    // Train the model.
-    let lin_reg = LinearRegression::new();
-    let model = lin_reg.fit(&dataset).unwrap();
-    model
-    // START TIMER (end in bin)
+  // START TIMER (end in bin)
+  let now = Utc::now().timestamp_nanos_opt().unwrap();
+  (setup, model, now)
 }
