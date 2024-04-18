b818f7083221fe835cd94556e32c07948076ecd7
Kinan Dak Albab <kinan.bab.14@gmail.com>
2024-04-18 15:40:47 -0400

    Update youchat

--- youchat/src/context.rs
+++ youchat/src/context.rs
@@ -2,18 +2,17 @@ use std::sync::{Arc, Mutex};
 use alohomora::AlohomoraType;
 use alohomora::{bbox::BBox, policy::NoPolicy};
 use alohomora::rocket::{BBoxRequest, BBoxRequestOutcome, FromBBoxRequest};
-use alohomora::context::Context; 
-use rocket::{State, http::Status};
-use rocket::outcome::IntoOutcome;
-use crate::backend::MySqlBackend;
+use alohomora::context::Context;
+use alohomora::db::{BBoxConn, BBoxOpts};
+use rocket::State;
 use crate::config::Config; 
 
 // the actual context data (just our backend)
 #[derive(AlohomoraType, Clone)]
-#[alohomora_out_type(verbatim = [db, config])] 
+#[alohomora_out_type(verbatim = [config])]
 pub struct ContextData {
-    pub user: Option<BBox<String, NoPolicy>>, // TODO(corinn) is this actually NoPolicy?
-    pub db: Arc<Mutex<MySqlBackend>>,
+    pub user: Option<BBox<String, NoPolicy>>,
+    pub db: Arc<Mutex<BBoxConn>>,
     pub config: Config,
 }
 
@@ -23,49 +22,37 @@ pub type YouChatContext = Context<ContextData>;
 // adapted from websubmit
 #[rocket::async_trait]
 impl<'a, 'r> FromBBoxRequest<'a, 'r> for ContextData {
-    type BBoxError = (); //TODO(corinn) use ContextDataError ?
+    type BBoxError = ();
 
     async fn from_bbox_request(
         request: BBoxRequest<'a, 'r>,
     ) -> BBoxRequestOutcome<Self, Self::BBoxError> {
-        let db: &State<Arc<Mutex<MySqlBackend>>> = request.guard().await.unwrap();
         let config: &State<Config> = request.guard().await.unwrap();
 
+        // Connect to the DB.
+        let mut db = BBoxConn::new(
+            // this is the user and password from the config.toml file
+            BBoxOpts::from_url(&format!("mysql://{}:{}@127.0.0.1/", config.db_user, config.db_password)).unwrap(),
+        ).unwrap();
+        db.query_drop("USE chats").unwrap();
 
         //from OG YouChat - params being segments from url
         let user = {
             match request.param(1)  {
                 // if the second param exists, that'll be the name
-                Some(param2) => {
-                    let boxed_name: BBox<_, NoPolicy> = param2.clone().unwrap();
-                    let name: String = boxed_name.discard_box();
-                    name.to_string().clone()
-                },
+                Some(param2) => param2.unwrap(),
                 // if not, the first param will be the name if we have one
-                None => {
-                    match request.param(0) {
-                        Some(param1) => {
-                            let a: BBox<String, NoPolicy> = param1.clone().unwrap();
-                            a.discard_box().to_string().clone()
-                        }
-                        None => "".to_string()
-                    }
+                None => match request.param(0) {
+                    Some(param1) => Some(param1.unwrap()),
+                    None => None,
                 }
             }
         };
 
-        request
-            .route()
-            .and_then(|_| {
-                Some(ContextData {
-                    user: Some(BBox::new(user, NoPolicy::new())),
-                    db: db.inner().clone(),
-                    config: config.inner().clone(),
-                })
-            })
-            .into_outcome((
-                Status::InternalServerError,
-                (),
-            ))
+        BBoxRequestOutcome::Success(ContextData {
+            user: user,
+            db: Arc::new(Mutex::new(db)),
+            config: config.inner().clone(),
+        })
     }
 }
