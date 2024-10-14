d5e287d31b431598121295a4b451532ecdebb023
Artem Agvanian <artemagvanian@icloud.com>
2024-10-14 12:08:24 -0400

    Changes related to scrutinizer update

--- alohomora_build/src/scrutinizer/mod.rs
+++ alohomora_build/src/scrutinizer/mod.rs
@@ -19,25 +19,6 @@ fn remove_vars(cmd: &mut Command) {
     }
 }
 
-fn generate_facts(env: &Env) {
-    warn!("\x1b[96mnote: \x1b[97mGenerating scrutinizer facts....\x1b[0m");
-
-    let mut cmd = Command::new(&env.cargo);
-    remove_vars(&mut cmd);
-
-    let status = cmd
-        .arg("+nightly-2023-04-12")
-        .arg("build")
-        .arg("-Zbuild-std=std,core,alloc,proc_macro")
-        .arg(&format!("--target={}", env.host))
-        .env("RUSTFLAGS", "-Zalways-encode-mir -Znll-facts")
-        .status()
-        .expect("\x1b[91merror: \x1b[97mFailed to generate scrutinizer facts'.\x1b[0m");
-    if !status.success() {
-        panic!("\x1b[91merror: \x1b[97mFailed to generate scrutinizer facts'.\x1b[0m");
-    }
-}
-
 // Run scrutinizer
 fn run_scrutinizer(env: &Env) {
     warn!("\x1b[96mnote: \x1b[97mRunning scrutinizer....\x1b[0m");
@@ -46,9 +27,11 @@ fn run_scrutinizer(env: &Env) {
     remove_vars(&mut cmd);
     
     let status = cmd
-        .arg("+nightly-2023-04-12")
+        .arg("+nightly-2023-08-25")
         .arg("scrutinizer")
         .args(["--config-path", "scrutinizer-config.toml"])
+        .env("RUST_BACKTRACE", "full")
+        .env("RUST_LOG", "scrutinizer=trace,scrutils=trace")
         .status()
         .expect("\x1b[91merror: \x1b[97mFailed to run scrutinizer'.\x1b[0m");
     if !status.success() {
@@ -60,7 +43,6 @@ pub fn scrutinize(env: &Env) {
     let profile = &env.profile;
     if profile == "release" {
         warn!("\x1b[97m    Scrutinizer\x1b[0m");
-        generate_facts(env);
         run_scrutinizer(env);
         warn!("\x1b[92m    Scrutinizer completed!\x1b[0m");
     } else {
