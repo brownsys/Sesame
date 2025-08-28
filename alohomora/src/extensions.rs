6a068940800500f3c991c79b4822c7dc39e91618
Kinan Dak Albab <kinan.bab.14@gmail.com>
2025-08-28 08:57:25 -0400

    Improve extensions API

--- alohomora/src/extensions.rs
+++ alohomora/src/extensions.rs
@@ -1,5 +1,9 @@
+use std::any::Any;
+
 use crate::bbox::BBox;
-use crate::policy::Policy;
+use crate::fold::fold;
+use crate::policy::{AnyPolicy, Policy, PolicyDyn};
+use crate::SesameType;
 
 // An extension is essentially a specific closure we allow to consume the internals of a BBox and
 // return an arbitrary data type (not necessarily protected).
@@ -16,22 +20,32 @@ use crate::policy::Policy;
 // Here instead, extensions can be defined over both base types and their references.
 // Because extensions are to be used sparingly and are expected to be written by the Tahini team
 // (or at least reviewed by them), we consider this an okay effort.
-pub trait SesamePConExtension<T, P: Policy, R>
-where
-    Self: Sized,
-{
+pub trait SesameExtension<T, P: Policy, R> where Self: Sized {
     fn apply(self, data: T, policy: P) -> R;
     fn apply_ref(self, data: &T, policy: &P) -> R;
 }
 
+// How extension is used.
 impl<T, P: Policy> BBox<T, P> {
-    pub fn apply_extension<R, E: SesamePConExtension<T, P, R>>(self, extension: E) -> R {
+    pub fn apply_extension<R, E: SesameExtension<T, P, R>>(self, extension: E) -> R {
         let (t, p) = self.consume();
         extension.apply(t, p)
     }
-    pub fn apply_extension_ref<R, E: SesamePConExtension<T, P, R>>(&self, extension: E) -> R {
+    pub fn apply_extension_ref<R, E: SesameExtension<T, P, R>>(&self, extension: E) -> R {
         let t = self.data();
         let p = self.policy();
         extension.apply_ref(t, p)
     }
+}
+
+// Apply extension to an entire SesameType.
+pub fn apply_extension<E, PDyn, T, R>(extension: E, object: T) -> Result<R, ()>
+where
+    T: SesameType<Any, PDyn>,
+    PDyn: PolicyDyn + ?Sized,
+    E: SesameExtension<T::Out, AnyPolicy<PDyn>, R>
+{
+    let folded = fold(object)?;
+    let (t, p) = folded.consume();
+    Ok(extension.apply(t, p))
 }
